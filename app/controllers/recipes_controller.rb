27f1f762b110afed9d164144a0cf8ec550fff007
Noah Birnel <nbirnel@gmail.com>
2014-04-24 15:07:02 -0700

    alternate layout for recipe
    add
      layout 'recipe'
    to app/controllers/recipes_controller.rb
    app/views/layouts/recipe.html.erb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,8 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
+  layout 'recipe'
+
   # GET /recipes
   # GET /recipes.json
   def index
