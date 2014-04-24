58d943505edf34b87454f7ce483aa9df45e19630
Noah Birnel <nbirnel@gmail.com>
2014-04-24 11:31:28 -0700

    add home page
    rails g controller Welcome hello
    add root 'welcome#hello' to config/routes.rb
    vim app/views/welcome/hello.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/hello'
+
   resources :recipes, except: :destroy
+
+  root 'welcome#hello'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
