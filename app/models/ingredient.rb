0f90733af9421113a16a6ded0d84b889db194a23
Noah Birnel <nbirnel@gmail.com>
2014-04-30 13:05:34 -0700

    add cheaper_than scope to ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,5 @@ class Ingredient < ActiveRecord::Base
   has_many :recipes, through: :quantities
 
   scope :cheap, -> { where('price_per_pound < 1.0') }
+  scope :cheaper_than, -> (price) { where('price_per_pound < ?', price.to_s) }
 end
