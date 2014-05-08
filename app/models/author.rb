b4308aa158ec30d709159c12ef902b3019fb4f4a
Noah Birnel <nbirnel@gmail.com>
2014-05-08 10:18:14 -0700

    pass validating author name

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ActiveRecord::Base
   has_many :recipes
+
+  validates :name, presence: true
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Author do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { Author.new() }
+  let(:author_named) { Author.new(name: 'Joe Schmoe') }
+
+  context 'name validation' do
+    it 'is invalid without a name' do
+      expect(author.valid?).to be_false
+    end
+
+    it 'is valid with a name' do
+      expect(author_named.valid?).to be_true
+    end
+
+  end
+
 end
+
