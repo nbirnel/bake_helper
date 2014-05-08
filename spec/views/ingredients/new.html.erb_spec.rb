f5bacd62e57df266c1da224ddb3d1a6bb9d206a4
Noah Birnel <nbirnel@gmail.com>
2014-05-08 14:47:09 -0700

    remove ingredient.quantity test

--- spec/views/ingredients/new.html.erb_spec.rb
+++ spec/views/ingredients/new.html.erb_spec.rb
@@ -4,7 +4,6 @@ describe "ingredients/new" do
   before(:each) do
     assign(:ingredient, stub_model(Ingredient,
       :name => "MyString",
-      :quantity => "MyString",
       :price_per_pound => "9.99"
     ).as_new_record)
   end
@@ -15,7 +14,6 @@ describe "ingredients/new" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", ingredients_path, "post" do
       assert_select "input#ingredient_name[name=?]", "ingredient[name]"
-      assert_select "input#ingredient_quantity[name=?]", "ingredient[quantity]"
       assert_select "input#ingredient_price_per_pound[name=?]", "ingredient[price_per_pound]"
     end
   end
